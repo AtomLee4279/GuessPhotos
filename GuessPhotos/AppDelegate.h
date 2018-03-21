532f794d9d34a59fc8cd2c9c3af6d7b04f43f206
李一贤 <liyixian@atomlee-imac.local>
2018-03-21 10:09:58 +0800

    Initial Commit

---
+++ GuessPhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuessPhotos
+//
+//  Created by 李一贤 on 2018/3/21.
+//  Copyright © 2018年 李一贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
